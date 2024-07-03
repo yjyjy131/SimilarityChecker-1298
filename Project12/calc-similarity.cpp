13a5613ed70385910206a157d28c9bcb865eadac
ye0nj.kim <fulmeach787@gmail.com>
2024-07-04 08:53:28 +0900

    [feat] legnth checker

--- Project12/calc-similarity.cpp
+++ Project12/calc-similarity.cpp
@@ -1,2 +1,34 @@
+#include <string>
+using namespace std;
 
+class SimilarityChecker {
+public:
+	SimilarityChecker(const string& answer) 
+		: answer{answer} {
+	}
 
+	int getScore(string target) {
+		int score = 0;
+		int answerLen = answer.size();
+		int targetLen = target.size();
+
+		if (answerLen == targetLen) {
+			return 60;
+		}
+
+		double longLen = max(answerLen, targetLen);
+		double shortLen = min(answerLen, targetLen);
+
+		if (longLen / shortLen >= 2) {
+			return 0;
+		}
+		
+		score = (1 - ((longLen - shortLen) / shortLen)) * 60;
+		if (score > 60) score = 60;
+
+		return score;
+	}
+
+private:
+	string answer;
+};
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,22 @@
 #include "pch.h"
 #include "../Project12/calc-similarity.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TestCaseName, whenHaveSameLength) {
+	SimilarityChecker checker{ "ASD" };
+	EXPECT_EQ(60, checker.getScore("DSA"));
+}
+
+TEST(TestCaseName, whenHaveLongerMoreThanTwiceLength) {
+	SimilarityChecker checker{ "A" };
+	EXPECT_EQ(0, checker.getScore("BB"));
+}
+
+TEST(TestCaseName, whenHaveDifferenctLength) {
+	SimilarityChecker checker{ "AAABB" };
+	EXPECT_EQ(20, checker.getScore("BAA"));
+}
+
+TEST(TestCaseName, whenHaveDifferenctLength2) {
+	SimilarityChecker checker{ "AA" };
+	EXPECT_EQ(30, checker.getScore("AAE"));
 }
